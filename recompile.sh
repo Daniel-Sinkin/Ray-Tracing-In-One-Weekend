659b96e03c1474ce9c959adf81b19849bdcca70d
Daniel-Sinkin <daniel.sinkin@gmx.de>
2024-09-14 18:08:52 +0200

    Added -o3 flags to render

--- recompile.sh
+++ recompile.sh
@@ -10,14 +10,17 @@ mkdir build
 cd build
 
 # Step 3: Generate the build system using CMake with Ninja generator
-cmake -G Ninja ..
+# Adding -DCMAKE_CXX_FLAGS_RELEASE="-O3" to enable the optimization level
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS_RELEASE="-O3" ..
 
 # Step 4: Build the project in Release mode
 cmake --build . --config Release --parallel
 
-# Step 5:
+# Step 5: Remove any previous image file
 rm ../image.ppm
+
+# Step 6: Run the project and output to the image file
 ./MyProject >> ../image.ppm
 
-# Step 6: Open the resulting image file using macOS Preview
+# Step 7: Open the resulting image file using macOS Preview
 open ../image.ppm
